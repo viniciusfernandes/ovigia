5891124b53fcc782269d844316c8e0d937ceb529
viniciusfernandes <viniciussf@hotmail.com>
2021-09-07 08:14:20 -0300

    Finalizacao do modal de confirmacao de ronda

--- src/screens/ronda/RondaVigiaScreen.js
+++ src/screens/ronda/RondaVigiaScreen.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+import { Modal, Pressable, StyleSheet, Text, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import TouchableButton from '../../components/TouchableButton';
 import matisse from '../../style/matisse';
@@ -9,7 +10,8 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         position: 'absolute',
         top: '80%',
-        width: '100%'
+        width: '100%',
+        zIndex: 1,
     },
     mapaContainer: {
         alignItems: 'center',
@@ -30,33 +32,95 @@ const styles = StyleSheet.create({
     },
 });
 
-export default () => (
-    <View style={styles.mapaContainer}>
-        <View style={styles.botoesContainer}>
-            <TouchableButton style={styles.pausarButton} styleText={{ color: 'white', fontSize: 20 }}
-                title="Pausar Ronda" />
-            <TouchableButton style={styles.concluirButton} styleText={{ fontSize: 20 }}
-                title="Concluir Ronda" />
-        </View>
-        <MapView
-            style={styles.mapa}
-            region={{
-                latitude: -23.70389,
-                longitude: -46.61829,
-                latitudeDelta: 0.015,
-                longitudeDelta: 0.0121,
-            }}
-        >
-            <Marker
-                coordinate={{
-                    latitude: -23.70389,
-                    longitude: -46.61829,
-                    latitudeDelta: 0.02,
-                    longitudeDelta: 0.02
-                }}
-                pinColor={"white"}
-                title={'Você está aqui!'}
-            />
-        </MapView>
-    </View>
-);
+const modalStyles = StyleSheet.create({
+    modalContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    modal: {
+        backgroundColor: "white",
+        borderRadius: 20,
+        elevation: 3,
+        alignItems: "center",
+        width: '50%',
+    },
+    simButton: {
+        backgroundColor: matisse.laranja,
+        color: 'white',
+        marginBottom: '10%',
+        width: '50%'
+    },
+    simText: {
+        color: "white",
+        fontSize: 15,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+    modalText: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        marginBottom: '10%',
+        marginTop: '10%',
+        textAlign: "center"
+    }
+});
+
+
+export default props => {
+    const [modalOpened, setModalOpened] = useState(false)
+
+    return (
+        <>
+            <View style={styles.botoesContainer}>
+                <TouchableButton style={styles.pausarButton} styleText={{ color: 'white', fontSize: 20 }}
+                    title="Pausar Ronda" />
+                <TouchableButton style={styles.concluirButton} styleText={{ fontSize: 20 }}
+                    title="Concluir Ronda" onPress={() =>
+                        setModalOpened(true)
+
+                    }
+                />
+            </View>
+            <View style={styles.mapaContainer}>
+
+                <MapView
+                    style={styles.mapa}
+                    region={{
+                        latitude: -23.70389,
+                        longitude: -46.61829,
+                        latitudeDelta: 0.015,
+                        longitudeDelta: 0.0121,
+                    }}
+                >
+                    <Marker
+                        coordinate={{
+                            latitude: -23.70389,
+                            longitude: -46.61829,
+                            latitudeDelta: 0.02,
+                            longitudeDelta: 0.02
+                        }}
+                        pinColor={"white"}
+                        title={'Você está aqui!'}
+                    />
+                </MapView>
+
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalOpened}
+
+                >
+                    <View style={modalStyles.modalContainer}>
+                        <View style={modalStyles.modal}>
+                            <Text style={modalStyles.modalText}>Confirma mesmo?</Text>
+                            <TouchableButton title='Sim' style={modalStyles.simButton}
+                                styleText={modalStyles.simText} onPress={() => setModalOpened(false)} />
+                        </View>
+                    </View>
+                </Modal>
+
+            </View>
+        </>
+    );
+}
